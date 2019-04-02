9afce3af3ac8e924c4390001389d78047b2f479d
Suprianto D <ersaka96@gmail.com>
2019-04-03 00:40:16 +0800

    status in pesanan detail, view list service sparepart added.
    status in pesanan detail, view list service sparepart added.

---
+++ app/src/main/java/com/ertohru/pthabgsm/ui/viewservispart/ViewServisPartActivity.kt
@@ -0,0 +1,12 @@
+package com.ertohru.pthabgsm.ui.viewservispart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewServisPartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_servis_part)
+    }
+}
---
+++ app/src/main/java/com/ertohru/pthabgsm/ui/viewservispart/ViewServisPartRecyclerView.kt
@@ -0,0 +1,2 @@
+package com.ertohru.pthabgsm.ui.viewservispart
+
