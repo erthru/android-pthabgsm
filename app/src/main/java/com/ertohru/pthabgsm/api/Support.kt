736d8392a2d649a1f7cf7259ce5faa43e9d771cc
Suprianto D <ersaka96@gmail.com>
2019-05-26 21:24:47 +0800

    change api to online

--- app/src/main/java/com/ertohru/pthabgsm/api/Support.kt
+++ app/src/main/java/com/ertohru/pthabgsm/api/Support.kt
@@ -4,7 +4,8 @@ class Support {
 
     companion object {
 
-        const val API_PTHABGSM = "http://192.168.56.1/anows/api_pthabgsm/"
+        //const val API_PTHABGSM = "http://192.168.56.1/anows/api_pthabgsm/"
+        const val API_PTHABGSM = "https://erthru.pro/pthabgsm/"
 
     }
 
